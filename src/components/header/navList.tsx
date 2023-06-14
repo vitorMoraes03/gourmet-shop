5ea178f9b0ff3608f67260857b2e26895026ae03
Vitor Moraes <vitoristvan@hotmail.com>
2023-06-14 18:50:03 -0300

    feat: setup blog

--- src/components/header/navList.tsx
+++ src/components/header/navList.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { MouseEventHandler, useState } from 'react';
+import Blog from './blog';
 
 const linksNav = [
   { href: '/blog', label: 'Blog' },
@@ -12,13 +14,25 @@ const linksNav = [
 ];
 
 function NavList() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  // corrigir o ANY
+  function handleHover(e: any) {
+    setIsHovered(e.target.value);
+  }
+
   return (
     <ul
       className="font-title p-3 md:font-subtitle md:text-xs 
     md:flex md:uppercase md:font-semibold md:ml-5"
     >
       {linksNav.map(({ href, label }) => (
-        <li key={`${href}${label}`} className="mb-2 pl-3">
+        <li
+          key={`${href}${label}`}
+          className="mb-2 pl-3"
+          onMouseEnter={handleHover}
+          value={label}
+        >
           <Link href={href}>{label}</Link>
         </li>
       ))}
