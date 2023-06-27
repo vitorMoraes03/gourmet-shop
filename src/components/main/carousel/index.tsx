701f1c8633a5ee2272ee8bcfbde6c8c19edad5b2
Vitor Moraes <vitoristvan@hotmail.com>
2023-06-27 08:54:53 -0300

    feat: initial setup carousel 2.0

--- src/components/main/carousel/index.tsx
+++ src/components/main/carousel/index.tsx
@@ -1,15 +1,10 @@
 'use client';
 
-import Image from 'next/image';
 import SingleSlide from './singleSlide';
 import { useRef, useState } from 'react';
+import { imagesObjs } from './imagesObjs';
 
 function Carousel() {
-  const [moveCarousel, setMoveCarousel] = useState<
-    'rigth' | 'left' | null
-  >(null);
-  const carouselRef = useRef<HTMLDivElement>(null);
-
   function handleBtnPrevious() {}
 
   function handleBtnNext() {}
@@ -18,42 +13,9 @@ function Carousel() {
     <div
       className="relative flex 
       transition-transform duration-500 ease-in-out h-96"
-      ref={carouselRef}
     >
-      <SingleSlide
-        src={
-          '/images/camille-brodard-f6Wpz1QPFZI-unsplash.jpg'
-        }
-        width={390}
-        height={244}
-        alt={'Alt para teste'}
-        paragraph={'Lorem ipsum'}
-        text={
-          'Amet consectetur adipisicing elit. Ex nostru voluptas sunt reprehenderit nam obcaecati, laboriosam quis sapiente?'
-        }
-        title={'Solor Sit'}
-        bgColor={'bg-orange'}
-        position={0}
-        moveCarousel={moveCarousel}
-        carouselRef={carouselRef}
-      />
-      <SingleSlide
-        src={
-          '/images/hermes-rivera-aK6WGqxyHFw-unsplash.jpg'
-        }
-        width={600}
-        height={400}
-        alt={'Alt para teste'}
-        paragraph={'Lorem ipsum'}
-        text={
-          'Amet consectetur adipisicing elit. Ex nostru voluptas sunt reprehenderit nam obcaecati, laboriosam quis sapiente?'
-        }
-        title={'Solor Sit'}
-        bgColor={'bg-green'}
-        position={1}
-        moveCarousel={moveCarousel}
-        carouselRef={carouselRef}
-      />
+      <SingleSlide imageObj={imagesObjs[0]} />
+      <SingleSlide imageObj={imagesObjs[1]} />
       <div className="inset-x-0 bottom-3 flex absolute justify-center gap-3 mt-6 z-20">
         <button
           className="border-black bg-black w-2 h-2 
--- src/components/main/carousel/singleSlide.tsx
+++ src/components/main/carousel/singleSlide.tsx
@@ -2,85 +2,50 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 interface SingleSlideProps {
-  src: string;
-  width: number;
-  height: number;
-  alt: string;
-  paragraph: string;
-  text: string;
-  title: string;
-  bgColor: string;
-  position: number;
-  moveCarousel: 'rigth' | 'left' | null;
-  carouselRef: React.RefObject<HTMLDivElement>;
+  imageObj: {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+    bgColor: string;
+    paragraph: string;
+    title: string;
+    text: string;
+  };
 }
 
-function SingleSlide({
-  src,
-  width,
-  height,
-  alt,
-  paragraph,
-  text,
-  title,
-  bgColor,
-  position,
-  moveCarousel,
-  carouselRef,
-}: SingleSlideProps) {
-  const [utilitiesCarousel, setUtilitiesCarousel] =
-    useState<string>('');
-
-  useEffect(() => {
-    const parent = carouselRef.current;
-    if (parent) {
-      carouselMovement(parent.offsetWidth);
-    } 
-  }, []);
-
-  // como fazer o alinhamento inicial?
-  // preciso ter um array que controla todos
-  // e a cada indice vamos movimentando x vezes o indice
-
-  // cada singleSlide tem que saber qual é a sua posição
-  // e ele vai receber um comando mover para a direita etc...
-
-  // pos * translate-x-full
-
-  function carouselMovement(width: number) {
-    const valueToMove = position * width;
-    setUtilitiesCarousel(`translate-x-[${valueToMove}px]`);
-  }
+function SingleSlide({ imageObj }: SingleSlideProps) {
 
   return (
     <div
-      className={`${utilitiesCarousel} ${position === 0 ? 'z-10' : ''} absolute 
-      grid grid-rows-2 md:grid-cols-2 h-96 md:h-96 shrink-0 w-full`}
+      className={
+        'absolute grid grid-rows-2 md:grid-cols-2 h-96 md:h-96 shrink-0 w-full'
+      }
     >
       <Image
-        src={src}
-        alt={alt}
-        width={width}
-        height={height}
+        src={imageObj.src}
+        alt={imageObj.alt}
+        width={imageObj.width}
+        height={imageObj.height}
         className="h-full"
       />
       <div
-        className={`${bgColor} grid p-4 pb-8 px-8 text-center 
+        className={`${imageObj.bgColor} grid p-4 pb-8 px-8 text-center 
         grid-auto-flow-column grid-auto-columns-1fr text-sm`}
       >
         <p className="uppercase text-xs -mb-2">
-          {paragraph}
+          {imageObj.paragraph}
         </p>
         <h2 className="text-2xl font-title">Solor Sit</h2>
         <p className="tracking-tight leading-4 px-7">
-          {text}
+          {imageObj.text}
         </p>
         <button
           type="button"
           className="bg-black text-white mt-2 py-1 uppercase 
           text-xs font-bold w-3/5 justify-self-center"
         >
-          {title}
+          {imageObj.title}
         </button>
       </div>
     </div>
