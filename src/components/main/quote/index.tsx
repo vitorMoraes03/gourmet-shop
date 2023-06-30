249a5e35b61f1d4c37900cb5960e921eba387089
Vitor Moraes <vitoristvan@hotmail.com>
2023-06-30 10:17:48 -0300

    feat: quote mobile

--- src/components/main/quote/index.tsx
+++ src/components/main/quote/index.tsx
@@ -1,13 +1,15 @@
 function Quote() {
   return (
     <div
-      className="flex flex-col md:flex-row justify-between w-full 
-    h-52 mb-14 md:py-12 md:px-28 bg-green font-title tracking-tight items-center"
+      className="my-8 flex w-full flex-wrap items-center
+      justify-between gap-3 bg-green 
+      px-8 py-6 font-title tracking-tight
+      md:mb-14 md:h-52 md:px-28 md:py-12"
     >
-      <h1 className="md:text-4xl w-1/4">
+      <h1 className="w-2/3 text-2xl md:w-1/4 md:text-4xl">
         <span className="italic">Lorem</span> ipsum tantric
       </h1>
-      <p className="md:text-3xl w-3/5">
+      <p className="md:w-3/5 md:text-2xl">
         Lorem ipsum dolor sit amet, consectetur adipisicing
         elit. Eaque aperiam consectetur, sit minima a
         incidunt, rem ut necessitatibus?
