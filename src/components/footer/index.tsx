6f2506f088b980f5db26829c50e6ae44856fb29c
Vitor Moraes <vitoristvan@hotmail.com>
2023-08-17 16:16:29 -0300

    feat: footer active

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import { FooterProps } from '../../../messages/useContent';
 function Footer({ content }: { content: FooterProps }) {
   return (
     <footer
-      className="absolute bottom-0 mb-2 flex w-full flex-wrap gap-3
+      className="mb-2 flex w-full flex-wrap gap-3
      bg-darkerGreen px-4 py-4 text-white md:mb-6 
      md:justify-between md:px-14 md:py-2"
     >
@@ -14,13 +14,14 @@ function Footer({ content }: { content: FooterProps }) {
         </p>
       </div>
       <div>
-        <ul className="flex flex-wrap gap-1 pr-10 md:flex-none md:pr-0">
+        <ul className="flex flex-wrap gap-1 pr-10 md:flex-none md:gap-3 md:pr-0">
           {content.links.map((item, index) => (
             <li
               key={`liFoot-${index}`}
               className='after:content-["|"] md:after:content-none'
             >
-              <Link href={item.href}>{item.label} </Link>
+              {item.label + ' '}
+              {/* <Link href={item.href}>{item.label} </Link> */}
             </li>
           ))}
         </ul>
--- src/components/main/quote/index.tsx
+++ src/components/main/quote/index.tsx
@@ -3,10 +3,10 @@ import { QuoteProps } from '../../../../messages/useContent';
 function Quote({ content }: { content: QuoteProps }) {
   return (
     <div
-      className="my-8 flex w-full flex-wrap items-center
-      justify-between gap-3 bg-gray 
-      px-8 py-6 font-title tracking-tight
-      md:mb-14 md:h-52 md:px-28 md:py-12"
+      className="my-8 flex w-full flex-wrap
+      items-center justify-between gap-3 
+      bg-zinc-200 px-8 py-6 font-title
+      tracking-tight md:mb-14 md:h-52 md:px-28 md:py-12"
     >
       <h1 className="w-2/3 text-2xl md:w-1/4 md:text-4xl">
         <span className="italic">{content.span} </span>
